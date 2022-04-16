dc4a9b24d00708a64aa654e70efe0c815b8d52f6
skoperst <>
2022-04-16 18:40:41 +0300

    monsters: implement state machine for monster

--- include/monster.h
+++ include/monster.h
@@ -4,11 +4,10 @@
 #include "tile.h"
 #include "soundfx.h"
 
-#define MONSTER_STATE_UNINITIALIZED        0
-#define MONSTER_STATE_STANDBY              1
-#define MONSTER_STATE_ACTIVE               2
-#define MONSTER_STATE_BURNING              3
-#define MONSTER_STATE_DEAD                 4
+#define MONSTER_STATE_BLINKING             0
+#define MONSTER_STATE_ACTIVE               1
+#define MONSTER_STATE_BURNING              2
+#define MONSTER_STATE_DEAD                 3
 
 typedef struct monster_struct {
     int state;
@@ -16,10 +15,13 @@ typedef struct monster_struct {
     int route_sz;
     int route_idx;
     int cooldown;
+    int ticks_in_state;
+    int on_fire;
     tile_t *tile;
 
     void (*tick)(struct monster_struct *monster);
     int (*is_dead)(struct monster_struct *monster);
+    int (*is_alive)(struct monster_struct *monster);
 } monster_t;
 
 monster_t* monster_create();
--- monster.c
+++ monster.c
@@ -12,32 +12,7 @@ int sun_path[] = {-2, 3, 1, 3, -2, 3, -4, 3, -5, -2, -2, -3, -5, 0, -4, 2, -4, 3
 int spider_path[] = {5, 3, 2, 4, 0, 5, -3, 4, -4, 4, -5, 3, -7, 2, -6, 2, -6, 1, -7, 1, -8, -2, -5, -3, -6, -3, -7, -2, -7, -3, -5, -2, -7, -3, -6, -3, -7, -3, -9, -2, -10, 0, -9, 1, -7, 2, -9, 2, -8, 3, -8, 0, -9, 0, -9, 1, -10, 2, -7, 0, -3, -1, -2, -4, 1, -4, 3, -3, 3, -1, 3, -1, 6, -2, 8, -2, 8, -1, 11, -1, 14, -1, 14, -2, 15, 0, 18, 0, 17, 0, 17, 1, 14, 1, 15, 1, 10, 2, 6, 2, 6, 2};
 
 
-static void monster_tick(monster_t *monster) {
-    printf("routex: %d, routey: %d \n", monster->route[monster->route_idx], monster->route[monster->route_idx+1]);
 
-    if (monster->cooldown > 4) {
-        monster->cooldown = 0;
-    }
-
-    if (monster->cooldown == 0) {
-        monster->tile->x += monster->route[monster->route_idx];
-        monster->tile->y += monster->route[monster->route_idx+1];
-        monster->route_idx += 2;
-        if (monster->route_idx >= monster->route_sz) {
-            monster->route_idx = 0;
-        }
-        monster->tile->sprite_idx++;
-        if (monster->tile->sprites[monster->tile->sprite_idx] == 0) {
-            monster->tile->sprite_idx = 0;
-        }
-    }
-
-    monster->cooldown++;
-}
-
-static int monster_get_sprite(tile_t *tile) {
-    return tile->sprites[tile->sprite_idx];
-}
 
 monster_t* monster_create_sun() {
     monster_t *monster = monster_create();
@@ -73,14 +48,111 @@ monster_t* monster_create_spider() {
     return monster;
 }
 
+static void monster_state_dead_routine(monster_t *monster) {
+    monster->ticks_in_state++;
+}
+
+static void monster_state_dead_enter(monster_t *monster) {
+    monster->state = MONSTER_STATE_DEAD;
+    monster->ticks_in_state = 0;
+}
+
+static void monster_state_burning_routine(monster_t *monster) {
+    monster->ticks_in_state++;
+    if (monster->ticks_in_state >= 100) {
+        monster_state_dead_enter(monster);
+    }
+}
+
+static void monster_state_burning_enter(monster_t *monster) {
+    monster->state = MONSTER_STATE_BURNING;
+    monster->cooldown = 0;
+    monster->ticks_in_state = 0;
+}
+
+static void monster_state_active_routine(monster_t *monster) {
+    if (monster->on_fire) {
+        monster_state_burning_enter(monster);
+        return;
+    }
+
+    if (monster->cooldown > 4) {
+        monster->cooldown = 0;
+    }
+
+    if (monster->cooldown == 0) {
+        monster->tile->x += monster->route[monster->route_idx];
+        monster->tile->y += monster->route[monster->route_idx+1];
+        monster->route_idx += 2;
+        if (monster->route_idx >= monster->route_sz) {
+            monster->route_idx = 0;
+        }
+        monster->tile->sprite_idx++;
+        if (monster->tile->sprites[monster->tile->sprite_idx] == 0) {
+            monster->tile->sprite_idx = 0;
+        }
+    }
+
+    monster->cooldown++;
+}
+
+static void monster_tick(monster_t *monster) {
+    printf("monster state: %d \n", monster->state);
+    printf("routex: %d, routey: %d \n", monster->route[monster->route_idx], monster->route[monster->route_idx+1]);
+
+    if (monster->state == MONSTER_STATE_ACTIVE) {
+        monster_state_active_routine(monster);
+    } else if (monster->state == MONSTER_STATE_BURNING) {
+        monster_state_burning_routine(monster);
+    } else if (monster->state == MONSTER_STATE_DEAD) {
+        monster_state_dead_routine(monster);
+    }
+
+}
+
+static int monster_get_sprite(tile_t *tile) {
+    int sprite = 0;
+
+    monster_t *monster = (monster_t *)tile->context;
+    if (monster->state == MONSTER_STATE_ACTIVE) {
+        sprite = tile->sprites[tile->sprite_idx];
+    } else if (monster->state == MONSTER_STATE_BURNING) {
+        if (monster->ticks_in_state > 100) {
+            sprite = 0;
+        } else if ((monster->ticks_in_state / 30) % 4 == 0) {
+            sprite = SPRITE_IDX_EXPLOSION3;
+        } else if ((monster->ticks_in_state / 30) % 4 == 1) {
+            sprite = SPRITE_IDX_EXPLOSION4;
+        } else if ((monster->ticks_in_state / 30) % 4 == 2) {
+            sprite = SPRITE_IDX_EXPLOSION1;
+        } else {
+            sprite = SPRITE_IDX_EXPLOSION2;
+        }
+    } else if (monster->state == MONSTER_STATE_DEAD) {
+        sprite = 0;
+    }
+
+    return sprite;
+}
+
+static int monster_is_alive(monster_t *monster) {
+    if (monster->state == MONSTER_STATE_ACTIVE) {
+        return 1;
+    }
+
+    return 0;
+}
+
 monster_t* monster_create() {
     monster_t *monster = malloc(sizeof(monster_t));
 
-    monster->state = MONSTER_STATE_UNINITIALIZED;
+    monster->state = MONSTER_STATE_ACTIVE;
     monster->route_idx = 0;
     monster->cooldown = 0;
-
+    monster->ticks_in_state = 0;
+    monster->on_fire = 0;
     monster->tick = &monster_tick;
+    monster->is_alive = &monster_is_alive;
 
     monster->tile = malloc(sizeof(tile_t));
     monster->tile->x = 0;
@@ -90,6 +162,7 @@ monster_t* monster_create() {
     monster->tile->mod = MONSTER;
     monster->tile->sprites[0] = 0;
     monster->tile->sprite_idx = 0;
+    monster->tile->context = monster;
     monster->tile->get_sprite = &monster_get_sprite;
 
     return monster;
